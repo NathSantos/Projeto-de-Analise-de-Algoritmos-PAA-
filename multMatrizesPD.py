294c82da72cefc9bac70c7f7d0bc419c185ede14
NathSantos <nathsantos2703@gmail.com>
2022-12-19 14:26:38 -0400

    Programação Dinâmica - multiplicação de matrizes

--- multMatrizesPD.py
+++ multMatrizesPD.py
@@ -2,19 +2,33 @@ def multMatrizPD(d, size):
     matPD = [[0 for x in range(size)] for y in range(size)]
     matK = [[0 for x in range(size)] for y in range(size)]
 
-    jIniciais = [i for i in range(1, size)]
+    jIniciais = [i for i in range(1, size)] # guarda os valores de j's (colunas) iniciais de cada diagonal que deve ser lida  
     
-    for j in range(len(jIniciais)):
-        j_aux = j+1
+    for j_aux in range(len(jIniciais)):
+        j = j_aux+1
         for i in range(len(jIniciais)):
-            print(f"[{i}],[{j_aux}]")
+            valoresM_K = {}
 
+            for k in range(i, j):
+                valoresM_K[matPD[i][k] + matPD[k+1][j] + (d[i]*d[k+1]*d[j+1])] = k+1
+
+            menor_valor = min(valoresM_K.keys())
+            valor_k = valoresM_K.get(menor_valor)
+
+            matPD[i][j] = menor_valor
+            matK[i][j] = valor_k
             
-            j_aux+=1
-
-
+            j+=1
         jIniciais.pop(0)
 
-d = [10,20,3,4,30]
+    print("===== Matriz de valores PD: =====")
+    for i in range(len(matPD)):
+        print(matPD[i])
+
+    print("===== Matriz de escolhas K: =====")
+    for i in range(len(matK)):
+        print(matK[i])
+
+d = [10,20,3,5,30,4]
 size = len(d)-1
 multMatrizPD(d, size)
